8051fb1cdd5fb17a991d90787fb08fb18d817b15
TreySchneider95 <tschneid95@gmail.com>
2023-02-15 12:20:32 -0800

    added controller

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,96 +2,20 @@ const express = require('express');
 const router = express.Router();
 // const blogValidate = require('../validation/blog');
 const Blog = require("./models/Blogs")
+const blogController = require("./controllers/blogController")
+
 // //instantiate mongodb 
 // const { db } = require('../mongo');
 
 // /* GET users listing. */
-router.get('/all', async function(req, res, next) {
-    const blogs = await Blog.find({})
+router.get('/all', blogController.getAllBlogs); 
 
-    res.json({
-      sucess:true,
-      blogs: blogs
-    });
+router.get('/single/:blogTitleToGet', blogController.getSingleBLog)
 
-    
-}); 
+router.delete('/single/:blogTitleToDelete', blogController.deleteBlog)
 
+router.post('/create-one', blogController.postBlog)
 
-
-// router.get('/single/:blogTitleToGet', async function(req, res, next){
-//     const blog = await db()
-//     .collection('sample_blogs')
-//     .find({title: req.params.blogTitleToGet})
-//     .toArray(function(err, result){
-//         if(err){
-//             res.status(400).send("Error fetching blog")
-//         }else{
-//             res.json(result)
-//         }
-//     })
-//     res.json({
-//         succsess:true,
-//         blog: blog
-//     })
-// })
-
-// router.delete('/single/:blogTitleToDelete', async function(req, res, next){
-//     const blog = await db()
-//     .collection('sample_blogs')
-//     .deleteOne({title: req.params.blogTitleToDelete}, function(err, result){
-//         if(err){
-//             res.status(400).send("Error deleting blog")
-//         }else{
-//             res.status(200).send({
-//                 success:true
-//             })
-//         }
-//     })
-//     res.json(blog)
-// })
-
-router.post('/create-one', async (req, res)=>{
-    const title = req.body.title
-    const text = req.body.text
-    const author = req.body.author
-    const categories = req.body.categories
-
-    const newBlog = new Blog({
-        title,
-        text,
-        author,
-        categories
-    })
-
-    const saveData = await newBlog.save()
-
-    res.json({
-        sucess: true,
-        blogs: saveData
-    })
-    res.json(newBlogValid)
-})
-
-// router.put('/update-one/:title', (req, res)=>{
-//     console.log(req.body)
-//     const blogValidResponse = blogValidate[1](req.body)
-//     if (blogValidResponse.success){
-//         blogValidResponse.updatedBlog.lastModified = new Date()
-//         db()
-//         .collection('sample_blogs')
-//         .updateOne({title: req.params.title}, req.body, function(err, result){
-//             if(err){
-//                 res.status(400).send("Error updating blog")
-//             }
-//         })
-//         res.json({
-//             success: true
-//         })
-//     }
-//     else{
-//         res.json(blogValidResponse)
-//     }
-// })
+router.put('/update-one/:title', blogController.putBlog)
 
 module.exports = router;
